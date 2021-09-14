faa061da70026bb9eef200e0c437bf898eb42ebe
Leonardo Telles <leo.c.telles@gmail.com>
2021-09-14 09:00:32 -0300

    Added TourRatingController

---
+++ src/main/java/com/example/ec/explorecali/web/TourRatingController.java
@@ -0,0 +1,32 @@
+package com.example.ec.explorecali.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ec.explorecali.repo.TourRatingRepository;
+import com.example.ec.explorecali.repo.TourRepository;
+
+/**
+ * Tour Rating Controller
+ *
+ * Created by Mary Ellen Bowman
+ */
+@RestController
+@RequestMapping(path = "/tours/{tourId}/ratings")
+public class TourRatingController {
+    TourRatingRepository tourRatingRepository;
+    TourRepository tourRepository;
+
+    @Autowired
+    public TourRatingController(TourRatingRepository tourRatingRepository, TourRepository tourRepository) {
+        this.tourRatingRepository = tourRatingRepository;
+        this.tourRepository = tourRepository;
+    }
+
+    protected TourRatingController() {
+
+    }
+
+
+}
