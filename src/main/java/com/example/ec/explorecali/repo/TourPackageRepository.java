6820a82d483703a281a150ca6f7684f92a0bfb39
Leonardo Telles <leo.c.telles@gmail.com>
2021-09-14 08:22:14 -0300

    Controlling API exposure

--- src/main/java/com/example/ec/explorecali/repo/TourPackageRepository.java
+++ src/main/java/com/example/ec/explorecali/repo/TourPackageRepository.java
@@ -3,11 +3,63 @@ package com.example.ec.explorecali.repo;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.explorecali.domain.TourPackage;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
 	Optional<TourPackage> findByName(String name);
+
+//	@Override
+//	@RestResource(exported = false)
+//	default <S extends TourPackage> S save(S entity) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	@RestResource(exported = false)
+//	default <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteById(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void delete(TourPackage entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteAllById(Iterable<? extends String> ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteAll(Iterable<? extends TourPackage> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
--- src/main/java/com/example/ec/explorecali/repo/TourRepository.java
+++ src/main/java/com/example/ec/explorecali/repo/TourRepository.java
@@ -4,11 +4,61 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.explorecali.domain.Tour;
 
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
 
 	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+//	@Override
+//	@RestResource(exported = false)
+//	default <S extends Tour> S save(S entity) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	@RestResource(exported = false)
+//	default <S extends Tour> Iterable<S> saveAll(Iterable<S> entities) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void delete(Tour entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteAllById(Iterable<? extends Integer> ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteAll(Iterable<? extends Tour> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}	
 	
 }
