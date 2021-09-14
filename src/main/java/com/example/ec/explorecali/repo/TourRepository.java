2e17511896bd755fa625e4e06603a72f0ea7cab0
Leonardo Telles <leo.c.telles@gmail.com>
2021-09-14 08:07:25 -0300

    Paging and sorting

--- src/main/java/com/example/ec/explorecali/repo/TourRepository.java
+++ src/main/java/com/example/ec/explorecali/repo/TourRepository.java
@@ -1,14 +1,14 @@
 package com.example.ec.explorecali.repo;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.example.ec.explorecali.domain.Tour;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
 
-	List<Tour> findByTourPackageCode(@Param("code") String code);
+	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 	
 }
