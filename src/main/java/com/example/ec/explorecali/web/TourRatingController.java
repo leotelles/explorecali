9c6ece7c3e8667c5da75f1a71fdb13d4abd1ac72
Leonardo Telles <leo.c.telles@gmail.com>
2021-09-15 08:09:34 -0300

    TourRatingController get mapping

--- src/main/java/com/example/ec/explorecali/web/TourRatingController.java
+++ src/main/java/com/example/ec/explorecali/web/TourRatingController.java
@@ -1,9 +1,13 @@
 package com.example.ec.explorecali.web;
 
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,6 +49,22 @@ public class TourRatingController {
     public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
     	Tour tour = verifyTour(tourId);
     	tourRatingRepository.save(new TourRating(new TourRatingPk(tour, ratingDto.getCustomerId()), ratingDto.getScore(), ratingDto.getComment()));
+    }
+    
+    @GetMapping
+    public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
+        verifyTour(tourId);
+        return tourRatingRepository.findByPkTourId(tourId).stream()
+                .map(RatingDto::new).collect(Collectors.toList());
+    }
+
+    @GetMapping(path = "/average")
+    public Map<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+        verifyTour(tourId);
+        return Map.of("average",tourRatingRepository.findByPkTourId(tourId).stream()
+                .mapToInt(TourRating::getScore).average()
+                .orElseThrow(() ->
+                new NoSuchElementException("Tour has no Ratings")));
     }
     
     /**
